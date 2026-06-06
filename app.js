bf360d12df03371a8f9b0edc36f7743af92262e9
Wai Yan Paing <waiyanpaing2304@gmail.com>
2026-06-06 13:15:25 +0630

    [fix] prepare the code for deployment

--- app.js
+++ app.js
@@ -12,9 +12,14 @@ const server = http.createServer(app);
 
 const roomProgressCache = {};
 
+const allowedOrigins = ["http://localhost:3000", "http://localhost:5173"];
+if (process.env.CLIENT_URL) {
+  allowedOrigins.push(process.env.CLIENT_URL);
+}
+
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://localhost:5173"],
+    origin: allowedOrigins,
     methods: ["GET", "POST", "PUT", "DELETE"]
   }
 });
